3c0608287b16437f05d69571b6e45977dcbfc8d3
Scott Ferguson <scott.ferguson82@gmail.com>
2020-05-05 15:11:30 -0500

    Updating the get-all file to take a json config file

--- scripts/Azure-DevOps/get-all.ps1
+++ scripts/Azure-DevOps/get-all.ps1
@@ -1,59 +1,68 @@
-# Read configuration file
-Get-Content "get-all.config" | foreach-object -begin {$h=@{}} -process { 
-    $k = [regex]::split($_,'='); 
-    if(($k[0].CompareTo("") -ne 0) -and ($k[0].StartsWith("[") -ne $True)) { 
-        $h.Add($k[0], $k[1]) 
-    } 
-}
-$baseUrl = $h.Get_Item("Url")
-$username = $h.Get_Item("Username")
-$personalAccessToken = $h.Get_Item("PersonalAccessToken")
+try {
+    # Import settings from config file
+    $config = Get-Content -Raw -Path "get-all.config" | ConvertFrom-Json
 
-# Retrieve list of all repositories
-$base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $username, $personalAccessToken)))
-$headers = @{
-    "Authorization" = ("Basic {0}" -f $base64AuthInfo)
-    "Accept" = "application/json"
-}
+    $baseUrl = $config.Url
+    $username = $config.Username
+    $personalAccessToken = $config.PersonalAccessToken
+    $isPullIfExists = $config.IsPullIfExists
 
-$projectsUrl = "$baseUrl/_apis/projects?api-version=4.0"
-$projects = Invoke-RestMethod -Uri $projectsUrl -Method Get -Headers $headers -ContentType application/json
-
-Write-Host "Project count found: " $projects.count -ForegroundColor Green
-$rootDirectory = get-location
-
-$projects.value.ForEach({
-
-    $projectName = $_.name
-
-    set-location $rootDirectory
-
-    if (!(test-path $projectName)) {
-        New-Item -ItemType Directory -Force -Path $projectName
+    # Retrieve list of all repositories
+    $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $username, $personalAccessToken)))
+    $headers = @{
+        "Authorization" = ("Basic {0}" -f $base64AuthInfo)
+        "Accept" = "application/json"
     }
 
-    $reposUrl = "$baseUrl/$projectName/_apis/git/repositories?api-version=4.0"
-    $repos = Invoke-RestMethod -Uri $reposUrl -Method Get -Headers $headers -ContentType application/json
+    $projectsUrl = "$baseUrl/_apis/projects?api-version=4.0"
+    $projects = Invoke-RestMethod -Uri $projectsUrl -Method Get -Headers $headers -ContentType application/json
 
-    Write-Host "Processing " $projectName -ForegroundColor Green
-    set-location $projectName
+    Write-Host "Project count found:" $projects.count -ForegroundColor Green
+    $rootDirectory = get-location
 
-    $repos.value.ForEach({
+    $projects.value.ForEach({
 
-        $repoName = $_.name
-        $repoUrl = $_.remoteUrl
-        $projectDirectory = get-location
+        $projectName = $_.name
 
-        if (!(Test-Path -Path $repoName)) {
-            Write-Host "Cloning " $repoName -ForegroundColor Green
+        set-location $rootDirectory
 
-            git clone $repoUrl --branch master --single-branch
-        } else {
-            Write-Host "Pulling " $repoName -ForegroundColor Yellow
-
-            set-location $repoName
-            git pull
-            set-location $projectDirectory
+        if (!(test-path $projectName)) {
+            New-Item -ItemType Directory -Force -Path $projectName
         }
+
+        $reposUrl = "$baseUrl/$projectName/_apis/git/repositories?api-version=4.0"
+        $repos = Invoke-RestMethod -Uri $reposUrl -Method Get -Headers $headers -ContentType application/json
+
+        Write-Host ""
+        Write-Host "Processing" $projectName -ForegroundColor Green
+        set-location $projectName
+
+        $repos.value.ForEach({
+
+            $repoName = $_.name
+            $repoUrl = $_.remoteUrl
+            $projectDirectory = get-location
+
+            if (!(Test-Path -Path $repoName)) {
+                Write-Host "Cloning" $repoName -ForegroundColor Green
+
+                git clone $repoUrl --branch master --single-branch -q
+            } elseif ($isPullIfExists -eq $true) {
+                Write-Host "Pulling" $repoName -ForegroundColor Yellow
+
+                set-location $repoName
+                git pull
+                set-location $projectDirectory
+            } else {
+                Write-Host "Already exists. Skipping" $repoName -ForegroundColor Yellow
+            }
+
+        })
     })
-})
+} catch {
+    Write-Host "An error occurred:" -ForegroundColor Red
+    Write-Host $_ -ForegroundColor Red
+    Write-Host $_.ScriptStackTrace -ForegroundColor Red
+
+    Read-Host -Prompt "Press Enter to exit"
+}
