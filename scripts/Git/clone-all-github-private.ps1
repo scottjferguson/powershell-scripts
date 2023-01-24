1297d0cff993cb4c8129af256425753bfb75a115
Scott Ferguson <scott.ferguson82@gmail.com>
2023-01-24 16:05:50 -0600

    Adding git scripts

---
+++ scripts/Git/clone-all-github-private.ps1
@@ -0,0 +1,49 @@
+try {
+    # Import settings from config file
+    $config = Get-Content -Raw -Path "get-all.config" | ConvertFrom-Json
+
+    $reposUrl = $config.Url
+    $username = $config.Username
+    $personalAccessToken = $config.PersonalAccessToken
+    $isPullIfExists = $config.IsPullIfExists
+
+    $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $username, $personalAccessToken)))
+    $headers = @{
+        "Authorization" = ("Basic {0}" -f $base64AuthInfo)
+        "Accept" = "application/json"
+    }
+
+    # Retrieve list of all repositories
+    $repos = Invoke-RestMethod -Uri $reposUrl -Method Get -Headers $headers -ContentType application/json
+
+    $repos.ForEach({
+
+        $repoName = $_.name
+        $repoUrl = $_.clone_url
+        $projectDirectory = get-location
+
+        if (!(Test-Path -Path $repoName)) {
+            Write-Host "Cloning" $repoName -ForegroundColor Green
+
+            git clone $repoUrl -v
+        } elseif ($isPullIfExists -eq $true) {
+            Write-Host "Pulling" $repoName -ForegroundColor Yellow
+
+            set-location $repoName
+            git pull
+            set-location $projectDirectory
+        } else {
+            Write-Host "Already exists. Skipping" $repoName -ForegroundColor Yellow
+        }
+
+    })
+} catch {
+    Write-Host "An error occurred:" -ForegroundColor Red
+    Write-Host $_ -ForegroundColor Red
+    Write-Host $_.ScriptStackTrace -ForegroundColor Red
+
+    Read-Host -Prompt "Press Enter to exit"
+}
+
+Write-Host ""
+read-host "Press ENTER to exit"
---
+++ scripts/Git/clone-all-github.ps1
@@ -0,0 +1,41 @@
+try {
+    # Import settings from config file
+    $config = Get-Content -Raw -Path "get-all.config" | ConvertFrom-Json
+
+    $reposUrl = $config.Url
+    $isPullIfExists = $config.IsPullIfExists
+
+    # Retrieve list of all repositories
+        $repos = Invoke-RestMethod -Uri $reposUrl -Method Get -ContentType application/json
+
+        $repos.ForEach({
+
+            $repoName = $_.name
+            $repoUrl = $_.clone_url
+            $projectDirectory = get-location
+
+            if (!(Test-Path -Path $repoName)) {
+                Write-Host "Cloning" $repoName -ForegroundColor Green
+
+                git clone $repoUrl -v
+            } elseif ($isPullIfExists -eq $true) {
+                Write-Host "Pulling" $repoName -ForegroundColor Yellow
+
+                set-location $repoName
+                git pull
+                set-location $projectDirectory
+            } else {
+                Write-Host "Already exists. Skipping" $repoName -ForegroundColor Yellow
+            }
+
+        })
+} catch {
+    Write-Host "An error occurred:" -ForegroundColor Red
+    Write-Host $_ -ForegroundColor Red
+    Write-Host $_.ScriptStackTrace -ForegroundColor Red
+
+    Read-Host -Prompt "Press Enter to exit"
+}
+
+Write-Host ""
+read-host "Press ENTER to exit"
